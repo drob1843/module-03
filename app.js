a6fee2363b6cc25cb749ecd5b64e1b5ca7c2a74e
drob1843 <94759314+drob1843@users.noreply.github.com>
2024-11-16 12:30:06 -0800

    this is for comp 692

--- app.js
+++ app.js
@@ -45,36 +45,95 @@ import http from 'http'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import fs from 'fs'
-import {} from 'dotenv/config'
+import { } from 'dotenv/config'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-            if (err) throw err
-            res.writeHead(200, { 'Content-Type': 'text/html' })
-            res.end(content)
-        })
-    } else if (req.url === '/about') {
-        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
-            if (err) throw err
-            res.writeHead(200, { 'Content-Type': 'text/html' })
-            res.end(content)
-        })
-    } else if (req.url === '/api/employees') {
-        const employees = [
-            {name: 'Sally Smith', title: 'CEO'},
-            {name: 'Frank Franklin', title: 'Director'},
-            {name: 'john Johnson', title: 'QA Manager'}
-        ]
-            res.writeHead(200, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify(employees))
-        
+    //build file path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
+
+    //lLot the filepath
+    // console.log(filepath)
+
+    // extension of file
+    let extname = path.extname(filePath)
+
+    //initial content type
+    let contentType = 'text/html'
+
+    //check extension set content type
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript'
+            break
+        case '.css':
+            contentType = 'text/css'
+            break
+        case '.json':
+            contentType = 'application/json'
+            break
+        case '.png':
+            contentType = 'image/png'
+            break
+        case '.jpg':
+            contentType = 'image/jpg'
+            break
     }
 
+    // check if content type is text/html but no .html file extension
+
+    if (contentType == 'text/html' && extname == '') {
+        filePath += '.html'
+    }
+    //read file
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code == 'ENOENT') {
+                //page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, { 'Content-Type': 'text/html' })
+                    res.end(content, 'utf8')
+                })
+            } else {
+                // some server error
+                res.writeHead(500)
+                res.end(`Server Error: ${err.code}`)
+            }
+        } else {
+            //Success
+            res.writeHead(200, { 'Content-Type': contentType })
+            res.end(content, 'utf8')
+        }
+    })
+
+
+    // if (req.url === '/') {
+    //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+    //         if (err) throw err
+    //         res.writeHead(200, { 'Content-Type': 'text/html' })
+    //         res.end(content)
+    //     })
+    // } else if (req.url === '/about') {
+    //     fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+    //         if (err) throw err
+    //         res.writeHead(200, { 'Content-Type': 'text/html' })
+    //         res.end(content)
+    //     })
 })
+
+// if (req.url === '/api/employees') {
+//     const employees = [
+//         {name: 'Sally Smith', title: 'CEO'},
+//         {name: 'Frank Franklin', title: 'Director'},
+//         {name: 'john Johnson', title: 'QA Manager'}
+//     ]
+//         res.writeHead(200, { 'Content-Type': 'application/json' })
+//         res.end(JSON.stringify(employees))
+
+// }
 
 const PORT = process.env.PORT || 5000
 
